c6bfa63016b0bf941cbbc5f0968816fa7a2c6fbb
estherkwizera46-hue <estherkwizera46@gmail.com>
2025-10-20 18:38:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,14 +11,29 @@ st.title("🌟 Personal Weight Prediction")
 st.write("Enter your details to predict your weight:")
 
 # Input fields
-gender = st.selectbox("Gender", [0, 1], format_func=lambda x: "Female" if x==0 else "Male")
+gender = st.selectbox("Gender", ['Male', 'Female'])
 age = st.number_input("Age", min_value=1, max_value=120, value=25)
 height = st.number_input("Height (m)", min_value=0.5, max_value=2.5, value=1.7)
-workout_type = st.selectbox("Workout Type", [0, 1, 2, 3], format_func=lambda x: f"Type {x}")
+workout_type = st.selectbox("Workout Type", ['Cardio', 'HIIT', 'Strength','Yoga'])
+
+gen=0
+type=0
+if gender=='Male':
+    gen=0
+else:
+    gen=1
+if workout_type == 'Cardio':
+    type=0
+elif workout_type == 'HIIT':
+    type=1
+elif workout_type== 'Strength':
+    type=2
+elif workout_type== 'Yoga':
+    type=3
 
 # Predict button
 if st.button("Predict Weight"):
-    Input=np.array([[gender,age,height,workout_type]])
+    Input=np.array([[gen,age,height,type]])
     input_data = pd.DataFrame([{
         'Gender': gender,
         'Age': age,
@@ -28,3 +43,4 @@ if st.button("Predict Weight"):
     
     predicted_weight = loaded_model.predict(Input)
     st.success(f"Predicted Weight: {predicted_weight[0]:.2f} kg")
+
